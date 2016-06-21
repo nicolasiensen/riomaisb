9fa91059fdc1ef8874efaa5591d58fdf5c1eb392
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-06-21 21:25:28 +0200

    Add toggle button for text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def toggle_button target_id, options = {}
+    raw (
+      "
+        <a
+          class='#{options[:class] || 'black regular btn btn-primary bg-white'}'
+          id='#{target_id}-btn'
+          onClick='
+            console.log(event)
+            document.getElementById(\"#{target_id}\").className = \"\";
+            document.getElementById(\"#{target_id}-btn\").className = \"hide\";
+            return false;
+          '>
+            Continuar lendo
+            <i class='fa fa-chevron-down ml1' aria-hidden='true'></i>
+        </a>
+      "
+    )
+  end
 end
